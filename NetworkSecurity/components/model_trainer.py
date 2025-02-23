3ee533fe76331d5a65a13f691754ed09e4d2f77a
Hammad Nasir <hammadnasir797@gmail.com>
2025-02-23 22:40:41 +0500

    Model Training with Hyperparameters Tunning and Evaluation Metrics has been Done

---
+++ NetworkSecurity/components/model_trainer.py
@@ -0,0 +1,140 @@
+import sys,os
+
+## Logging And Exception
+from NetworkSecurity.exception.exception import NetworkSecurityException
+from NetworkSecurity.logging.logger import logging
+
+## configuration file for data Config
+from NetworkSecurity.entity.config_entity import ModelTrainerConfig
+from NetworkSecurity.entity.artifact_entity import ModelTrainerArtifact,DataTransformationArtifact
+
+from NetworkSecurity.utils.main_utils.utils import save_pickle,load_pickle,load_numpy_array,evaluate_model
+from NetworkSecurity.utils.ml_utils.metric.classification_metric import get_classification_score
+from NetworkSecurity.utils.ml_utils.model.estimator import NetworkModel
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import (
+    RandomForestClassifier,
+    GradientBoostingClassifier,
+    AdaBoostClassifier
+)
+
+
+
+
+
+class ModelTrainer:
+    def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
+        try:
+            self.model_trainer_config=model_trainer_config
+            self.data_transformation_artifact=data_transformation_artifact
+
+        except Exception as e:
+            raise NetworkSecurityException (e,sys) from e
+
+    ## Creating model Function
+    def train_model(self,x_train,y_train,x_test,y_test):
+        try:
+            models = {
+                    "Decision Tree": DecisionTreeClassifier(),
+                    "Random Forest": RandomForestClassifier(),
+                    'Logistic Regression': LogisticRegression(),
+              
+                }
+
+            params = {
+                    "Decision Tree": { "criterion": ["gini", "entropy"] },
+                    "Random Forest": { "criterion": ["gini", "entropy"] },
+                    "Logistic Regression": { "penalty": [ "l2"] },
+                }
+
+           
+
+            ## Evaluating 
+            model_report:dict=evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models,params=params)
+            print(model_report)
+
+            ## Selecting Model
+            best_model_score=max(sorted(model_report.values()))
+
+            ## Getting Best Model Name 
+            best_model_name=list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+             ]
+
+            best_model=models[best_model_name]
+
+            logging.info(f"Best Model Found on both training and testing dataset")
+
+            y_train_pred=best_model.predict(x_train)
+
+            train_model_score=get_classification_score(y_actual=y_train,y_pred=y_train_pred)
+            ## Track Model
+
+            y_test_pred=best_model.predict(x_test)
+
+            test_model_score=get_classification_score(y_actual=y_test,y_pred=y_test_pred)
+
+            ## Load model
+            preprocessor=load_pickle(self.data_transformation_artifact.transformed_object_file_path)
+
+            model_dir_path=os.path.dirname(self.model_trainer_config.trained_model_file_path)
+            os.makedirs(model_dir_path,exist_ok=True)
+
+
+            network_model=NetworkModel(
+                preprocessor=preprocessor,
+                model=best_model
+              )
+
+            save_pickle(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=network_model
+              )
+
+            model_trainer_artifact=ModelTrainerArtifact(
+                trained_model_file_path=self.model_trainer_config.trained_model_file_path,
+                train_metric_artifact=train_model_score,
+                test_metric_artifact=test_model_score
+              )
+
+            logging.info(f"Model Trainer Artifact: {model_trainer_artifact}")
+                
+            return model_trainer_artifact
+                
+
+
+        except Exception as e:
+            raise NetworkSecurityException (e,sys) from e
+
+        
+    def intiate_model_trainer(self) -> ModelTrainerArtifact:
+        try:
+            ## Loading train and test file path
+            train_file_path=self.data_transformation_artifact.transformed_train_file_path
+            test_file_path=self.data_transformation_artifact.transformed_test_file_path
+            print("Model Model")
+            print(train_file_path)
+
+            ## Loading Train and Test Numpy Array
+            train_array=load_numpy_array(train_file_path)
+            test_array=load_numpy_array(test_file_path)
+
+
+            ## Splitting Data
+            x_train, y_train, x_test, y_test=(
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+               )
+
+
+            ## Creating Model
+            model_trainer_artifact=self.train_model(x_train,y_train,x_test,y_test)
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise NetworkSecurityException (e,sys) from e
--- NetworkSecurity/utils/main_utils/utils.py
+++ NetworkSecurity/utils/main_utils/utils.py
@@ -9,6 +9,9 @@ import yaml
 import pickle
 #import dill
 import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+
 
 ## Read Yaml File
 def read_yaml_file(file_path: str) -> dict:
@@ -46,6 +49,15 @@ def save_numpy_array(file_path:str, array:np.array):
         raise NetworkSecurityException(e,sys) from e
 
 
+## Loading Numpy Array
+def load_numpy_array(file_path: str) -> np.array:
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return np.load(file_obj, allow_pickle=True)  
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+
+
 ## SavePickel File
 def save_pickle(file_path:str ,obj : object) -> None:
     try:
@@ -59,3 +71,46 @@ def save_pickle(file_path:str ,obj : object) -> None:
         raise NetworkSecurityException (e,sys) from e
 
 
+## Loading Pickle
+def load_pickle(file_path:str,) ->object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file :{file_path} doesnot exists")
+        
+        with open(file_path,'rb') as file_obj:
+            print(file_obj)
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise NetworkSecurityException (e,sys) from e
+    
+
+def evaluate_model(x_train,y_train,x_test,y_test,models,params):
+    try:
+        report = {}
+         
+        for i in range(len(list(models))):
+
+            model = list(models.values())[i]
+            model_params = params[list(models.keys())[i]]
+
+
+            gs = GridSearchCV(model, model_params, cv=3 , error_score="raise")
+            gs.fit(x_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train, y_train)
+
+            y_train_pred = model.predict(x_train)
+
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report        
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
